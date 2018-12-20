16b8d02420e2400faf3d641003bde29eeec0f3e7
usbpendrive <ferry.fernando@gmail.com>
2018-12-21 06:13:57 +0700

    New module using scaffold.

---
+++ __manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open academy module for managing trainings:
+        - training courses
+        - training sessions
+        - attendee registration
+    """,
+
+    'author': "Fernando",
+    'website': "https://github.com/usbpendrive/openacademy",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
