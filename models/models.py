7f74282e6ca07580372f9e944902ef997addd0e4
usbpendrive <ferry.fernando@gmail.com>
2018-12-21 07:28:17 +0700

    Session gantt view.

--- models/models.py
+++ models/models.py
@@ -61,6 +61,8 @@ class Session(models.Model):
     end_date = fields.Date(string="End Date", store=True,
                            compute='_get_end_date', inverse='_set_end_date')
 
+    hours = fields.Float(string="Duration in hours", computer='_get_hours', inverse='_set_hours')
+
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for r in self:
@@ -85,6 +87,15 @@ class Session(models.Model):
                     'message': "Increase seats or remove excessive attendees.",
                 },
             }
+
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours / 24
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
